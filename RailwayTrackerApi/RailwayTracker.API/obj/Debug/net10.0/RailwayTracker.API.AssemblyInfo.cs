b4d3c405a4455860a286d18ce727d7989a62ebb9
Yusuf Umer <yusufumer26@gmail.com>
2026-09-11 22:24:43 +0300

    feat: add result pattern for application responses

--- RailwayTrackerApi/RailwayTracker.API/obj/Debug/net10.0/RailwayTracker.API.AssemblyInfo.cs
+++ RailwayTrackerApi/RailwayTracker.API/obj/Debug/net10.0/RailwayTracker.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RailwayTracker.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cbc2c3aa3bb45bbdfae1e03897c466b2640de2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75faa60f1d52595e087ec2e98e98d0bf8bfb5402")]
 [assembly: System.Reflection.AssemblyProductAttribute("RailwayTracker.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RailwayTracker.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
