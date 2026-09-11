70270347c7caab085176bfa6cbea4bc227939b8b
Yusuf Umer <yusufumer26@gmail.com>
2026-09-11 22:32:03 +0300

    test: add custom web application factory

---
+++ RailwayTrackerApi/RailwayTracker.Tests/Integration/CustomWebApplicationFactory.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using RailwayTracker.Infrastructure.Persistence;
+
+namespace RailwayTracker.Tests.Integration;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            services.RemoveAll<DbContextOptions<AppDbContext>>();
+            services.RemoveAll<AppDbContext>();
+
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseInMemoryDatabase("RailwayTrackerTestDb"));
+        });
+    }
+}
