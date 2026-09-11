663db12a9e6f6f07af125a84e4592dc8c0531801
Yusuf Umer <yusufumer26@gmail.com>
2026-09-11 22:07:54 +0300

    feat: add train domain entity

--- RailwayTrackerApi/RailwayTracker.API/obj/Debug/net10.0/RailwayTracker.API.AssemblyInfo.cs
+++ RailwayTrackerApi/RailwayTracker.API/obj/Debug/net10.0/RailwayTracker.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RailwayTracker.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c9b3bbb9bf7a2b0ed3314c171137f6e04e12c16")]
 [assembly: System.Reflection.AssemblyProductAttribute("RailwayTracker.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RailwayTracker.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ RailwayTrackerApi/RailwayTracker.Domain/Entities/Train.cs
@@ -0,0 +1,14 @@
+namespace RailwayTracker.Domain.Entities;
+
+public class Train
+{
+    public int Id { get; set; }
+    public string Code { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public TrainStatus Status { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public DateTime LastUpdated { get; set; }
+
+    public ICollection<Arrival> Arrivals { get; set; } = new List<Arrival>();
+}
