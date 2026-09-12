922284641ccf79aadee5e393c4f20b902eb20acd
Yusuf Umer <yusufumer26@gmail.com>
2026-09-12 22:08:10 +0300

    feat: add API train position broadcaster

--- RailwayTrackerApi/RailwayTracker.Domain/obj/Debug/net10.0/RailwayTracker.Domain.AssemblyInfo.cs
+++ RailwayTrackerApi/RailwayTracker.Domain/obj/Debug/net10.0/RailwayTracker.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RailwayTracker.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95f34c5840a6b2fef9faf5068920b997ee271922")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+391b657479217585fa1346830c30f5d6166f96ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("RailwayTracker.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RailwayTracker.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
