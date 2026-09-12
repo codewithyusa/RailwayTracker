b60d79ac613181b89244e9b0a535f1ad1b568a0a
Yusuf Umer <yusufumer26@gmail.com>
2026-09-12 23:27:14 +0300

    feat: add get all announcements repository method

--- RailwayTrackerApi/RailwayTracker.Application/Common/Interfaces/IAnnouncementRepository.cs
+++ RailwayTrackerApi/RailwayTracker.Application/Common/Interfaces/IAnnouncementRepository.cs
@@ -4,6 +4,7 @@ namespace RailwayTracker.Application.Common.Interfaces;
 
 public interface IAnnouncementRepository
 {
+    Task<IEnumerable<Announcement>> GetAllAsync(CancellationToken ct = default);
     Task<IEnumerable<Announcement>> GetByStationAsync(int stationId, CancellationToken ct = default);
     Task AddAsync(Announcement announcement, CancellationToken ct = default);
 }
