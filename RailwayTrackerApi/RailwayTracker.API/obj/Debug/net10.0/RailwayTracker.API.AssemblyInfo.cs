282ea99c317c0dd99868d4928f566559a8126c23
Yusuf Umer <yusufumer26@gmail.com>
2026-09-12 21:39:26 +0300

    chore: add EF Core relational package

--- RailwayTrackerApi/RailwayTracker.API/obj/Debug/net10.0/RailwayTracker.API.AssemblyInfo.cs
+++ RailwayTrackerApi/RailwayTracker.API/obj/Debug/net10.0/RailwayTracker.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RailwayTracker.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+704e158addc60b99ead3d521b42262f72a0af23c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc3fd75c784323e999ce69f5aa9bffb8c30ee015")]
 [assembly: System.Reflection.AssemblyProductAttribute("RailwayTracker.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RailwayTracker.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
