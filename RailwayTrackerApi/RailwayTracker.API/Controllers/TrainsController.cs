ee51e8f92c00dd6326bc9c406315e0f5426cefa3
Yusuf Umer <yusufumer26@gmail.com>
2026-09-12 22:15:16 +0300

    feat: add train position update endpoint

--- RailwayTrackerApi/RailwayTracker.API/Controllers/TrainsController.cs
+++ RailwayTrackerApi/RailwayTracker.API/Controllers/TrainsController.cs
@@ -1,6 +1,8 @@
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using RailwayTracker.Application.Trains.Queries.GetAllTrains;
+using RailwayTracker.Application.Trains.Queries.GetTrainById;
+using RailwayTracker.Application.Trains.Commands.UpdateTrainPosition;
 
 namespace RailwayTracker.API.Controllers;
 
@@ -17,4 +19,22 @@ public class TrainsController : ControllerBase
         var result = await _mediator.Send(new GetAllTrainsQuery(), ct);
         return Ok(result.Value);
     }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(int id, CancellationToken ct)
+    {
+        var result = await _mediator.Send(new GetTrainByIdQuery(id), ct);
+        if (!result.IsSuccess) return NotFound(result.Error);
+        return Ok(result.Value);
+    }
+
+    [HttpPut("{id}/position")]
+    public async Task<IActionResult> UpdatePosition(int id, [FromBody] UpdatePositionRequest request, CancellationToken ct)
+    {
+        var result = await _mediator.Send(new UpdateTrainPositionCommand(id, request.Latitude, request.Longitude), ct);
+        if (!result.IsSuccess) return NotFound(result.Error);
+        return Ok();
+    }
 }
+
+public record UpdatePositionRequest(double Latitude, double Longitude);
--- RailwayTrackerApi/RailwayTracker.Application/obj/Debug/net10.0/RailwayTracker.Application.AssemblyInfo.cs
+++ RailwayTrackerApi/RailwayTracker.Application/obj/Debug/net10.0/RailwayTracker.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RailwayTracker.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+922284641ccf79aadee5e393c4f20b902eb20acd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+871fd0179b5a2f38de26a5759ab947ddc0bde891")]
 [assembly: System.Reflection.AssemblyProductAttribute("RailwayTracker.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RailwayTracker.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
