3ba9b0a5d3225398902d09f4ae406f8492a4621b
Yusuf Umer <yusufumer26@gmail.com>
2026-09-17 12:03:44 +0300

    feat(domain): add Train entity

--- RailwayTrackerApi/RailwayTracker.Domain/Entities/Train.cs
+++ RailwayTrackerApi/RailwayTracker.Domain/Entities/Train.cs
@@ -1,5 +1,3 @@
-using RailwayTracker.Domain.Enums;
-
 namespace RailwayTracker.Domain.Entities;
 
 public class Train
@@ -7,11 +5,9 @@ public class Train
     public int Id { get; set; }
     public string Code { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
-    public TrainStatus Status { get; set; }
+    public int LineId { get; set; }
+    public Line? Line { get; set; }
     public double Latitude { get; set; }
     public double Longitude { get; set; }
-    public DateTime LastUpdated { get; set; } = DateTime.UtcNow;
-
-    public ICollection<Arrival> Arrivals { get; set; } = new List<Arrival>();
-    public ICollection<Schedule> Schedules { get; set; } = new List<Schedule>();
+    public bool IsActive { get; set; }
 }
