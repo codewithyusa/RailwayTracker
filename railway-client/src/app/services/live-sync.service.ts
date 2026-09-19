83d8c1a0e3504eb166d37863c17be469bfd134da
Yusuf Umer <yusufumer26@gmail.com>
2026-09-19 21:46:31 +0300

    feat: add SignalR live train position sync

---
+++ railway-client/src/app/services/live-sync.service.ts
@@ -0,0 +1,42 @@
+import { inject, Injectable, signal, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
+import { Subject } from 'rxjs';
+
+export interface TrainPositionEvent {
+  trainId: number;
+  latitude: number;
+  longitude: number;
+}
+
+@Injectable({ providedIn: 'root' })
+export class LiveSyncService {
+  private platformId = inject(PLATFORM_ID);
+  private connection: HubConnection | null = null;
+  private positionSubject = new Subject<TrainPositionEvent>();
+
+  positions$ = this.positionSubject.asObservable();
+  connectionState = signal<'connected' | 'reconnecting' | 'disconnected'>('disconnected');
+
+  connect() {
+    if (this.connection) return;
+    if (!isPlatformBrowser(this.platformId)) return;
+
+    this.connection = new HubConnectionBuilder()
+      .withUrl('http://localhost:5285/hubs/railway')
+      .withAutomaticReconnect([0, 2000, 10000, 30000])
+      .build();
+
+    this.connection.on('ReceiveTrainPosition', (trainId: number, lat: number, lng: number) => {
+      this.positionSubject.next({ trainId, latitude: lat, longitude: lng });
+    });
+
+    this.connection.onreconnecting(() => this.connectionState.set('reconnecting'));
+    this.connection.onreconnected(() => this.connectionState.set('connected'));
+    this.connection.onclose(() => this.connectionState.set('disconnected'));
+
+    this.connection.start()
+      .then(() => this.connectionState.set('connected'))
+      .catch(err => console.error('SignalR error:', err));
+  }
+}
