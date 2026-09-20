b9a024a8c4055ead3356f1ff38b70f4809bf2a1e
Yusuf Umer <yusufumer26@gmail.com>
2026-09-20 20:59:28 +0300

    refactor: remove provideZoneChangeDetection from app config

--- railway-client/src/app/app.config.ts
+++ railway-client/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -6,7 +6,6 @@ import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withComponentInputBinding()),
     provideHttpClient(),
     provideAnimationsAsync(),
